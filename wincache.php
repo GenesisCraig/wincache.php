e4d9efc993e32749208ce82e4bce28f244b68834
Craig Anderson <GenesisCraig@users.noreply.github.com>
2018-06-27 15:26:35 -0400

    fixed username

--- wincache.php
+++ wincache.php
@@ -50,7 +50,7 @@ define('PASSWORD', 'wincache');
  * In that case use the array below to define the names of users in your
  * domain/network/workgroup which you want to grant access to.
  */
-$user_allowed = array('genesis\craiga', 'DOMAIN\user2', 'DOMAIN\user3');
+$user_allowed = array('DOMAIN\user1', 'DOMAIN\user2', 'DOMAIN\user3');
 
 /**
  * If the array contains string 'all', then all the users authenticated by IIS
